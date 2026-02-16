489c5ea38e7023e68ec4050e67733443307b6e1a
liyaxing <lurenpluto@gmail.com>
2026-02-15 22:58:34 -0800

    feat: 控制台运行时同步输出日志到终端

--- service/src/logging.rs
+++ service/src/logging.rs
@@ -1,9 +1,11 @@
 use std::fs::OpenOptions;
+use std::io::IsTerminal;
 use std::path::PathBuf;
 
 use chrono::Local;
 use tracing::{error, info};
 use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
 use tracing_subscriber::EnvFilter;
 
 pub fn init_logging(
@@ -39,29 +41,45 @@ pub fn init_logging(
         })?;
 
     let (non_blocking, guard) = tracing_appender::non_blocking(log_file);
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(log_level));
+    let console_enabled = std::io::stdout().is_terminal();
+    let init_result = if console_enabled {
+        tracing_subscriber::fmt()
+            .with_env_filter(
+                EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(log_level)),
+            )
+            .with_target(true)
+            .with_line_number(true)
+            .with_file(true)
+            .with_thread_ids(true)
+            .with_ansi(false)
+            .with_writer(std::io::stdout.and(non_blocking))
+            .try_init()
+    } else {
+        tracing_subscriber::fmt()
+            .with_env_filter(
+                EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(log_level)),
+            )
+            .with_target(true)
+            .with_line_number(true)
+            .with_file(true)
+            .with_thread_ids(true)
+            .with_ansi(false)
+            .with_writer(non_blocking)
+            .try_init()
+    };
 
-    tracing_subscriber::fmt()
-        .with_env_filter(env_filter)
-        .with_target(true)
-        .with_line_number(true)
-        .with_file(true)
-        .with_thread_ids(true)
-        .with_ansi(false)
-        .with_writer(non_blocking)
-        .try_init()
-        .map_err(|e| {
-            let msg = format!("Could not initialize logging subscriber: {}", e);
-            error!("{}", msg);
-            msg
-        })?;
+    init_result.map_err(|e| {
+        let msg = format!("Could not initialize logging subscriber: {}", e);
+        error!("{}", msg);
+        msg
+    })?;
 
     info!(
         service_name,
         pid,
         log_dir = %log_dir_path.display(),
         log_file = %file_path.display(),
+        console_enabled,
         "logging initialized"
     );
 
